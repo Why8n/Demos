e326640bc6612d0dd7cb5e76b042f9d81dadd066
Why8n <Why8n@gmail.com>
2021-10-08 17:01:44 +0800

    feat: let go specific pages

--- SpringSecurityDemo/src/main/java/com/yn/springsecuritydemo/config/SpringSecurityConfig.java
+++ SpringSecurityDemo/src/main/java/com/yn/springsecuritydemo/config/SpringSecurityConfig.java
@@ -1,17 +1,27 @@
 package com.yn.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+// Spring Security 自定义配置类
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 所有授权请求都必须先进行认证
-        http.authorizeRequests((request) -> request.anyRequest().authenticated());
-        // 开启 HTTP Basic 认证
-         http.httpBasic();
+        http.authorizeRequests()
+                // 放行接口 login/**，/test/index
+                .antMatchers("/login/**","/test/index").permitAll()
+                // 其余所有请求都必须经过认证
+                .anyRequest().authenticated()
+                .and()
+                // 使用 HTTP Basic 认证
+                .httpBasic()
+                .and()
+                // 同时支持表单认证（网页端优先使用表单认证）
+                .formLogin();
     }
 }
