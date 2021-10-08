a0d168c92325b23d97d47614924a2e908fb4ac59
Why8n <Why8n@gmail.com>
2021-10-08 23:54:32 +0800

    feat: update password encoder

--- SpringSecurityDemo/src/main/java/com/yn/springsecuritydemo/config/SecurityPasswordEncoder.java
+++ SpringSecurityDemo/src/main/java/com/yn/springsecuritydemo/config/SecurityPasswordEncoder.java
@@ -3,12 +3,13 @@ package com.yn.springsecuritydemo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityPasswordEncoder {
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
