81534de69dd1447389f89afb6361f2e3c70519a2
Why8n <Why8n@gmail.com>
2021-10-08 22:48:28 +0800

    feat: in memory UserDetailsService by configure HttpSecurity

--- SpringSecurityDemo/src/main/java/com/yn/springsecuritydemo/config/SpringSecurityConfig.java
+++ SpringSecurityDemo/src/main/java/com/yn/springsecuritydemo/config/SpringSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.yn.springsecuritydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 // Spring Security 自定义配置类
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -18,7 +25,22 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests((request) ->
-                request.anyRequest().permitAll());
+        super.configure(http);
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String password = this.passwordEncoder.encode("123456");
+        // 内存用户
+        auth.inMemoryAuthentication()
+                // 用户名
+                .withUser("whyn")
+                // 密码
+                .password(password)
+                // 角色
+                .roles("admin")
+                // 权限 CRUD
+                .authorities("create","read","udpate","delete");
     }
 }
