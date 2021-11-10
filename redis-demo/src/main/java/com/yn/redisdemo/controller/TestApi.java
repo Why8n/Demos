8d1997d9daf6a420c005ca8c0bf2bc7e40526212
Why8n <Why8n@gmail.com>
2021-11-11 03:19:03 +0800

    feat: using Jedis as Redis client

---
+++ redis-demo/src/main/java/com/yn/redisdemo/controller/TestApi.java
@@ -0,0 +1,16 @@
+package com.yn.redisdemo.controller;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestApi {
+
+    @GetMapping
+    public String index() {
+        return "Hello Redis";
+    }
+}
