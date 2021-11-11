088c893ad8ab898b12c6436622fbdee63dee7823
Why8n <Why8n@gmail.com>
2021-11-12 01:31:16 +0800

    feat: basic demo done

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+Spring Boot integrates Redis demo.
--- redis-demo/src/test/java/com/yn/redisdemo/RedisDemoApplicationTests.java
+++ redis-demo/src/test/java/com/yn/redisdemo/RedisDemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.yn.redisdemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class RedisDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    // 注入 RedisTemplate
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+    @Test
+    public void testRedis() {
+        redisTemplate.opsForValue().set("username", "Whyn");
+        String username = (String) redisTemplate.opsForValue().get("username");
+        Assert.isTrue("Whyn".equals(username),"username not the same!");
+        System.out.println(username);
+    }
 }
